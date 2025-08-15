04bd2bc320b4102424946b90e3beb17d5944d95c
anitej-c-501 <anitejchanda26@gmail.com>
2025-08-15 09:55:40 -0500

    Changed some measurements

--- SmartMeterApi/obj/Debug/net8.0/SmartMeterApi.AssemblyInfo.cs
+++ SmartMeterApi/obj/Debug/net8.0/SmartMeterApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartMeterApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b90755c63cbfe89cc5691789eecf60e7491cbd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62b965fe020748b12369e200706e92d73b2b5f80")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartMeterApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartMeterApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
